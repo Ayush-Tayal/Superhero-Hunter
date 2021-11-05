5c70d4ad62ef01fab739d399d0c248bdec46d886
Ayush Tayal <ayushtayal1111@gmail.com>
2021-11-05 16:59:54 +0530

    call done

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.querySelector("#input");
+const seachResult = document.querySelector(".search-result");
+input.onkeyup =  debounced(supereherofunc, 500);
+
+
+async function supereherofunc(){
+    let inputVal = input.value;
+    const response = await fetch(`https://www.superheroapi.com/api.php/3023624931288669/search/${inputVal}`);
+    const data = await response.json();
+    console.log(data);
+    
+    const superheroArray = data.results;
+    console.log(superheroArray);
+    
+    inputVal.length > 0 ?
+    superheroArray.forEach((superhero, idx)=>{  
+        seachResult.innerHTML += `<li> ${superhero.name}</li>`
+                                    
+    }):`${seachResult.innerHTML = ""}`
+    
+}
+
+function debounced(para, delay) {
+    let timer;
+
+    const debouncedFunction = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        supereherofunc();
+      }, delay);
+    };
+
+    return debouncedFunction;
+}
+    
